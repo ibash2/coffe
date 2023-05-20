710fa49089c7e113116646259f88a68b0a05ffab
Rashid <@rashid_rashidov_04@mail.ru>
2023-05-20 18:25:58 +0300

    nigo

--- coffe_project/lib/screens/product_page.dart
+++ coffe_project/lib/screens/product_page.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../Widget/product.dart';
 
 
+class Product_information extends StatefulWidget {
+  const Product_information({super.key});
+
+  @override
+  State<Product_information> createState() => _Product_informationState();
+}
+
+class _Product_informationState extends State<Product_information> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: Text( '\$${Product.cost}'),
+      ),
+    );
+  }
+}
