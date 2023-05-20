de534aa0dd2b9102f6066bf18c26604f5fa4d9ac
Ibas2 <ibragim.020205@gmail.com>
2023-05-20 12:53:09 +0300

    chototo

---
+++ coffe_project/lib/screens/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+// ignore: camel_case_types
+class Home_page extends StatelessWidget {
+  const Home_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('sdfkd'),
+      )
+    );
+  }
+}
