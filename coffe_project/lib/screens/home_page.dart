62df82af30ed32393a57a259058797c3591885a0
Rashid <@rashid_rashidov_04@mail.ru>
2023-05-20 16:37:53 +0300

    kll

--- coffe_project/lib/screens/home_page.dart
+++ coffe_project/lib/screens/home_page.dart
@@ -13,8 +13,8 @@ class Home_page extends StatefulWidget {
 }
 
 class _Home_pageState extends State<Home_page> {
-  int indexpage= 0;
- List<Product> productList =[
+  int indexpage = 0;
+  List<Product> productList = [
     Product('images/stakan1.jpg', "Kapushino", 100, 20),
     Product('images/stakan2.jpg', "Nigo", 200, 30),
     Product('images/stakan3.jpg', "GelMunna", 300, 40),
@@ -22,48 +22,51 @@ class _Home_pageState extends State<Home_page> {
   ];
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.people_outline,color: Colors.black,),
-          centerTitle: true,
-          title: const Text("Name",style: TextStyle(color: Colors.black),),
-          actions: [
-            Icon(Icons.shopping_basket_rounded,color: Colors.black,),
-          ],
-          elevation: 0,
-          backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        leading: Icon(
+          Icons.people_outline,
+          color: Colors.black,
         ),
-        body: SizedBox(
-          height: 250,
-          child: ScrollSnapList(
-            itemBuilder: _buildListItem,
-            itemCount: productList.length,
-            itemSize: 150,
-            onItemFocus: (index) {},
-            dynamicItemSize: true,
+        centerTitle: true,
+        title: const Text(
+          "Name",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          Icon(
+            Icons.shopping_basket_rounded,
+            color: Colors.black,
           ),
-          
+        ],
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SizedBox(
+        height: 250,
+        child: ScrollSnapList(
+          itemBuilder: _buildListItem,
+          itemCount: productList.length,
+          itemSize: 150,
+          onItemFocus: (index) {},
+          dynamicItemSize: true,
         ),
-        bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: Colors.grey,
-          color: Colors.grey.shade200,
-          animationDuration: Duration(milliseconds: 300),
-          items: [
-            Icon(
-              Icons.coffee,
-              color: Colors.black
-
-            ),
-            Icon(
-              Icons.donut_small_sharp,
-            ),
-            Icon(
-              Icons.coffee_maker,
-              color: Colors.black,
-            ),
-          ],
-        ),
-        
-        );
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.grey,
+        color: Colors.grey.shade200,
+        animationDuration: Duration(milliseconds: 300),
+        items: [
+          Icon(Icons.coffee, color: Colors.black),
+          Icon(
+            Icons.donut_small_sharp,
+          ),
+          Icon(
+            Icons.coffee_maker,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildListItem(BuildContext context, int index) {
@@ -75,38 +78,43 @@ class _Home_pageState extends State<Home_page> {
         elevation: 12,
         child: ClipRRect(
           borderRadius: const BorderRadius.all(Radius.circular(10)),
-          child: Column(
-            children: [
-              Image.asset(
-                product.imagePate,
-                fit: BoxFit.cover,
-                width: 150,
-                height: 180,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                product.title,
-                style: const TextStyle(fontSize: 15),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '\$${product.cost}',
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      '${product.reviewCount} Reviews',
-                      style: const TextStyle(color: Colors.blue),
-                    )
-                  ],
+          child: MaterialButton(
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onPressed: () {},
+            child: Column(
+              children: [
+                Image.asset(
+                  product.imagePate,
+                  fit: BoxFit.cover,
+                  width: 150,
+                  height: 180,
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  product.title,
+                  style: const TextStyle(fontSize: 15),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '\$${product.cost}',
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '${product.reviewCount} Reviews',
+                        style: const TextStyle(color: Colors.blue),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
