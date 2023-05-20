3ff1826878647313edc0a39a3aad13699eaba634
Rashid <@rashid_rashidov_04@mail.ru>
2023-05-20 15:37:43 +0300

    Nigo

---
+++ coffe_project/lib/Widget/product.dart
@@ -0,0 +1,8 @@
+class Product{
+  final String imagePate;
+  final String title;
+  final double cost;
+  final int reviewCount;
+
+  Product(this.imagePate,this.title,this.cost,this.reviewCount);
+}
